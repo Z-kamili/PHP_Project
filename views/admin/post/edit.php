a45799fd0011db8fa18953af6581bd3852c62b30
Z-Kamili <zakariakamili97@gmail.com>
2022-07-07 22:07:44 +0100

    add some feature

--- views/admin/post/edit.php
+++ views/admin/post/edit.php
@@ -38,10 +38,10 @@ if(!empty($_POST)) {
 
          $date = $_POST['created_at'];
          $post->setName($_POST['name'])
-         ->setContent($_POST['content'])
-         ->setSlug($_POST['slug']);
-         $post->setCreated_at($date);
-         $post->setId($params['id']);
+              ->setContent($_POST['content'])
+              ->setSlug($_POST['slug']);
+        $post->setId($params['id']);
+        $post->setCreated_at($date);
          $postTable->update($post,'post');
          $success = true;
 
